b32997ef1aaeaefa1438b95a4a24db8a0ebe970d
yorrid van groningen <yorrid08@gmail.com>
2023-04-07 14:49:34 +0200

    created geopandas df

--- eindpresentatie/main.py
+++ eindpresentatie/main.py
@@ -3,4 +3,5 @@ import pandas as pd
 import plotly.express as px
 import yfinance as yf
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
+import geopandas as gpd
+import requests
